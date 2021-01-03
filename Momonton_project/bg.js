4063fb23a9b6d64863287ec2b435dd722b65f42e
DG <thsdimaker@gmail.com>
2021-01-04 00:27:19 +0900

    (Fin)2020.01.04

--- Momonton_project/bg.js
+++ Momonton_project/bg.js
@@ -2,12 +2,11 @@ const body = document.querySelector("body");
 
 const IMG_NUMBER = 3;
 
-
-
 function paintImage(imgNumber){
     const image = new Image();
 
-    image.src = `/images/${imgNumber + 1}.jpg`;
+    image.src = `images/${imgNumber + 1}.jpg`;
+    image.classList.add("bgImage");
     body.appendChild(image);
 
 }
